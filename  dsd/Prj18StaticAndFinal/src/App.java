aabdaa9bfecf6bf801199c19f9b788b02969eb54
RILEYB <RILEYB@USL-1509.BIRLASOFT.COM>
2020-10-14 16:01:59 -0500

    Finished

---  dsd/Prj18StaticAndFinal/src/App.java
+++  dsd/Prj18StaticAndFinal/src/App.java
@@ -1,5 +1,7 @@
 class Thing {
 	
+	public final static int LUCKY_NUMBER = 7;
+	
 	// Instance Variables
 	public String name;
 
@@ -7,10 +9,24 @@ class Thing {
 	public static String description;
 	
 	
-	public void showName() {
-		System.out.println(name);
+	// Determine the number of time objects are created.
+	public static int count = 0;
+	
+	public int id;
+	
+	public Thing() {
+		id = count;
+		count++;
+		
 	}
 	
+	// Instance methods CAN access the static data.
+	public void showName() {
+		System.out.println("Object id: " + id + ", " + description + ": " + name);
+		//System.out.println(description);
+	}
+	
+	// Static methods CANNOT output instance variables. 
 	public static void shoInfo() {
 		System.out.println(description);
 	 
@@ -26,22 +42,32 @@ public class App {
 
 		// Access by the class 
 		// Static methods can access static data because they both belong to the class. 
-		// Static methods CANNOT output instance variables. 
-		// Instance methods CAN access the static data.
+	
+		
 	    Thing.shoInfo();
-		
-		System.out.println(Thing.description);
-		
+	    
+	    
+	    System.out.println("Before creating objects, count is: " + Thing.count);
 		
 		Thing thing1 = new Thing();
 		Thing thing2 = new Thing();
+		Thing thing3 = new Thing(); 
 		
+		System.out.println("After creating objects count is: " + Thing.count);
+		   
 		thing1.name = "Bob";
 		thing2.name = "Sue";
 		
 	    thing1.showName();
 	    thing2.showName();
+	    thing3.showName();
 	    
+	    System.out.println(Math.PI);
+	    
+	    
+	    System.out.println(Thing.LUCKY_NUMBER);
+	    
+	 
 	  
 	    
 		
